a643a1a6a39d959b05a0df8de72b5f82d037a34d
Seasonbrucelee <sezen39@hotmail.com>
2022-06-07 14:28:40 +0200

    modif AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin', name: 'admin_')]
 class AdminController extends AbstractController
 {
+    /**HOME PAGE DU FUTRE BACK-OFFICE**/
     #[Route('/', name: 'home')]
     public function index(): Response
     {
@@ -16,4 +20,21 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+    #[Route('/category/add', name: 'category_add')]
+    /** (Request $request) = Objet Request et injection de dépendance */
+    public function addCategory(Request $request): Response
+    {
+        //dd($request);
+
+        // Instace de Category 
+        $category = new Category();
+        //dd($category);
+        //On demande de fabriquer un formulaire. en mémoire de préfabriquer un contenu HTML sur la base de ce que l'on a mit dans le fichier formulaire PHP
+        $form = $this->createForm(CategoryType::class, $category);
+        dd($form);
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'Category Add',
+        ]);
+    }
 }
+
